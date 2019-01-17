1e7c7720bbf1e9ca4eb07626963c68f36b0fec9c
徐阳杰 <xuyangjie@xuyangjiedeMacBook-Pro.local>
2019-01-17 12:16:52 +0800

    upate

--- wechat/autoReply.js
+++ wechat/autoReply.js
@@ -11,13 +11,60 @@ function autoReply(message,ctx) {
                 ToUserName: message.FromUserName[0],
                 FromUserName: message.ToUserName[0],
                 MsgType: 'text',
-                Content: 'Hello!!'
+                Content: '欢迎进入喵喵桃公众号'
             });
             ctx.body = xml
         }else if (message.Event[0] === 'unsubscribe') {
             console.log('取关');
             return Promise.resolve('');
+        }else if (message.Event[0] === 'LOCATION') {
+            const xml =  createXML({
+                ToUserName: message.FromUserName[0],
+                FromUserName: message.ToUserName[0],
+                MsgType: 'text',
+                Content: `您的地理位置：${message.Latitude[0]},${message.Longitude[0]}`
+            });
+            ctx.body = xml
+        }else if (message.Event[0] === 'CLICK') {
+            const xml =  createXML({
+                ToUserName: message.FromUserName[0],
+                FromUserName: message.ToUserName[0],
+                MsgType: 'text',
+                Content: `您点击了菜单：${message.EventKey[0]}`
+            });
+            ctx.body = xml
+        }else if (message.Event[0] === 'SCAN') {
+            // const xml =  createXML({
+            //     ToUserName: message.FromUserName[0],
+            //     FromUserName: message.ToUserName[0],
+            //     MsgType: 'text',
+            //     Content: `您点击了菜单：${message.EventKey[0]}`
+            // });
+            // ctx.body = xml
+        }else if (message.Event[0] === 'VIEW') {
+            const xml =  createXML({
+                ToUserName: message.FromUserName[0],
+                FromUserName: message.ToUserName[0],
+                MsgType: 'text',
+                Content: `您点击了菜单中的链接：${message.EventKey[0]}`
+            });
+            ctx.body = xml
         }
+    }else if(message.MsgType[0] === 'text'){
+        const content = message.Content[0]
+        let reply = '喵了个咪的！！'
+        if(content=='1'){
+            reply = '心里烦啊'
+        }else if(content=='2'){
+            reply = '来学前端'
+        }
+        const xml =  createXML({
+            ToUserName: message.FromUserName[0],
+            FromUserName: message.ToUserName[0],
+            MsgType: 'text',
+            Content: reply
+        });
+        ctx.body = xml
     }
 }
 
